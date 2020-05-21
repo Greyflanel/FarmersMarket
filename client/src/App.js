4608571cbdec365d030795a82b2b441c80a99342
Greyflanel <47545416+Greyflanel@users.noreply.github.com>
2020-05-20 19:34:03 -0500

    moved images into src/assets

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './assets/caseofstrawberries-johnny-martinez-lBIFDzYwbDo-unsplash.jpg';
 import './App.css';
 
 function App() {
