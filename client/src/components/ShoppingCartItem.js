45f9878357cba876eedc2047fab686fee811b351
Greyflanel <47545416+Greyflanel@users.noreply.github.com>
2020-09-04 16:44:18 -0500

    Remove from cart is now operating properly.

--- client/src/components/ShoppingCartItem.js
+++ client/src/components/ShoppingCartItem.js
@@ -1,18 +1,21 @@
-import React from 'react';
+import React, { useContext}from 'react';
+import {CartContext} from '../Contexts/CartContext';
 
 const ShoppingCartItem = props => {
+const [cart, setCart] = useContext(CartContext);
 
-    // const removeFromCart = props => {
-        
-    // }
-
+    const removeFromCart = () => {
+      setCart(cart.filter((item) => item.product !== props.product))
+      
+    };
+    
     return (
         <div className="shopping-cart-item">
             <h2>{props.product}</h2>
             <img src={props.product_image_url} alt={props.product}/>
             <h5>${props.price}</h5>
             <div>
-                <button>Remove from Cart</button>
+                <button onClick={removeFromCart}>Remove from Cart</button>
             </div>
         </div>
     );
