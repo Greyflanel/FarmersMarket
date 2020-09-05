30649db2be4c325f5f0f4f52567397819d1c04d1
Greyflanel <47545416+Greyflanel@users.noreply.github.com>
2020-09-05 17:45:50 -0500

    added code to avoid duplicate entries to shopping cart.

--- client/src/components/SingleProduct.js
+++ client/src/components/SingleProduct.js
@@ -4,14 +4,15 @@ import { CartContext } from "../Contexts/CartContext.js";
 
 const SingleProduct = (props) => {
   const [cart, setCart] = useContext(CartContext);
-  
+
   if (props.singleProduct.length > 0) {
     const { product, product_image_url, price  } = props.singleProduct[0];
     const addItem = () => {
     const cartItem = { product: props.singleProduct[0].product,
        price: props.singleProduct[0].price,
         product_image_url: props.singleProduct[0].product_image_url }
-    setCart([...cart, cartItem]);
+        const noDuplicates = cart.filter(item => item.product !== product)
+          setCart([...noDuplicates, cartItem]);
   };
   
     return (
@@ -21,7 +22,7 @@ const SingleProduct = (props) => {
         <img className="cartItem" src={product_image_url} alt={product_image_url} />
         <h3>PricePerPound: ${price}</h3><br/>
         <button onClick={addItem}>Add to Cart</button>
-        <div className="testCircle">Text inside a circle</div>
+        <div className="testCircle"></div>
         
       </div>
       </div>
