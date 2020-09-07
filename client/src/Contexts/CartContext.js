44028e93990976359a91b4373fc4b7e71c459700
Greyflanel <47545416+Greyflanel@users.noreply.github.com>
2020-09-07 17:46:28 -0500

    added shopping cart to local storage to persist data on logout or refresh

--- client/src/Contexts/CartContext.js
+++ client/src/Contexts/CartContext.js
@@ -1,9 +1,15 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
 
 export const CartContext = createContext();
 
 export const CartProvider = (props) => {
-    const [cart, setCart] = useState([]);
+    const [cart, setCart] = useState( () => {
+      const localData = localStorage.getItem('cart');
+      return localData ? JSON.parse(localData) : [];
+    });
+    useEffect(() => {
+      localStorage.setItem('cart', JSON.stringify(cart))
+    }, [cart])
   return <CartContext.Provider value={[cart, setCart]}>
             {props.children}
         </CartContext.Provider>;
