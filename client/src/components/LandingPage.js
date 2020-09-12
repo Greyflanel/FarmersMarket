5dedfdbfe89110497775bab4e27e4d30f1b94201
Greyflanel <47545416+Greyflanel@users.noreply.github.com>
2020-09-11 23:11:27 -0500

    saving design changes for navbar

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -10,7 +10,7 @@ const LandingPage = () => {
     return (
       <div className="small-farm">
         <h3>LANDING PAGE</h3>
-        <figure class="circle"></figure>
+        
         {/* <img src={farm} alt="organic farm sign with red barn  " />
         <img src={organic} alt="100% organic sign" /> */}
         {/* <div>
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import farmdesign from "../assets/farmdesign.png";
+import farmdesign from "../assets/hydro-logos.png";
 import shoppingCart from "../assets/cart2.svg";
 
 const NavBar = () => {
@@ -8,7 +8,22 @@ const NavBar = () => {
     return (
       <>
         <div className="navbar">
-          <h1 className="hydro-h1">HYDROPONIC</h1>
+          {/* <h1 className="hydro-h1">HYDROPONIC</h1> */}
+          <div className="nav-container">
+            <h1 className="hydro-h1" id="simple_arc">
+              <span class="w0">H</span>
+              <span class="w1">Y</span>
+              <span class="w2">D</span>
+              <span class="w3">R</span>
+              <span class="w4">O</span>
+              <span class="w5">P</span>
+              <span class="w6">O</span>
+              <span class="w7">N</span>
+              <span class="w8">I</span>
+              <span class="w9">C</span>
+            </h1>
+          </div>
+          <figure class="circle"></figure>
           <h1>FARMERS MARKET</h1>
           <NavLink className="nav-links" to="/products">
             PRODUCTS
@@ -16,18 +31,20 @@ const NavBar = () => {
           <NavLink className="nav-links" to="/">
             HOME
           </NavLink>
-          <NavLink className="nav-links" to="/authorize">
-            LOGIN
-          </NavLink>
-          <NavLink className="nav-links" to="/admin">
+
+          {/* <NavLink className="nav-links" to="/admin">
             ADMIN
-          </NavLink>
+          </NavLink> */}
           <NavLink to="/cart" className="shopping-cart-icon">
             <img src={shoppingCart} alt="shopping cart icon" />
           </NavLink>
+          {/* <NavLink className="login-button" to="/authorize">
+          LOGIN
+        </NavLink> */}
 
           {/* <img src={farmdesign} alt="farming logo with fruits and vegetables" /> */}
         </div>
+        
       </>
     );
 };
