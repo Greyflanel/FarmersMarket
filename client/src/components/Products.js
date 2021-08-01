183b71f8d766e12028f6e7e00a618fbfc4d0efbf
Greyflanel <47545416+Greyflanel@users.noreply.github.com>
2021-08-01 11:28:42 -0500

    got horizontal scroll to work on product list

--- client/src/components/Products.js
+++ client/src/components/Products.js
@@ -4,35 +4,31 @@ import { NavLink } from "react-router-dom";
 import "../styles/product_list.css";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-gsap.registerPlugin(ScrollTrigger)
+gsap.registerPlugin(ScrollTrigger);
 
 const Products = () => {
   const {productsList} = useContext(ProductContext);
-  const boxRef = useRef(null);
   
+    useEffect(() => {
+      const mouseWheel = document.querySelector(".horizontal-scroll");
 
-  useEffect(() => {
-    
-    
-    gsap.to([boxRef.current], {
-      x: () =>
-      console.log(boxRef.current)
-        - (boxRef.current - document.documentElement.clientWidth) + "px",
-      ease: "none",
-      horizontal: true,
-      scrollTrigger: {
-        scroller: ".horizontal-scroll",
-        trigger: [boxRef.current],
-        scrub: 1,
-      },
-    });
-    });
-  
+  mouseWheel.addEventListener("wheel", function (e) {
+    const scroll = 100; // How many pixels to scroll
+
+    if (e.deltaY > 0)
+      // Scroll right
+      mouseWheel.scrollLeft += scroll;
+    // Scroll left
+    else mouseWheel.scrollLeft -= scroll;
+    e.preventDefault();
+  });
+      
+    }, []);
 
 
   return (
-    <section ref={boxRef}>
-      <div  className="horizontal-scroll">
+    <section >
+      <div className="horizontal-scroll">
         {productsList.map((product) => (
           <NavLink
             to={`/products/${product.id}`}
@@ -41,13 +37,13 @@ const Products = () => {
           >
             <div className="flowers">
               <div className="circle"></div>
-              <img src={product.product_image_url} alt={product.product} />
+              <img className="product-list-image" src={product.product_image_url} alt={product.product} />
             </div>
             <div className="info">
               <h1 className="title">Adidas</h1>
               <h3>EXCEPTION COMFORT.</h3>
               <div className="sizes">
-                <button>39</button> <button>40</button>{" "}
+                <button>39</button> <button>40</button>
                 <button className="active"></button> <button>44</button>
               </div>
               <div className="purchase">
