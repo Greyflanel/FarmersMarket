649cf6667cff27f5ca00e9ace8fb9b1d133fff5e
Greyflanel <47545416+Greyflanel@users.noreply.github.com>
2021-09-26 12:11:14 -0500

    adjusting auth

--- src/Contexts/AuthContext.js
+++ src/Contexts/AuthContext.js
@@ -4,12 +4,9 @@ export const AuthContext = createContext();
 
 export const AuthProvider = props => {
     const [auth, setAuth] = useState({
-        email: '',
-        role: '',
-        isLoggedIn: false,
-        isAdmin: false,
+        
     });
-
+    
     return (
         <AuthContext.Provider value={[auth, setAuth]}>
             {props.children}
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext, createContext } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { NavLink, Redirect } from "react-router-dom";
 import axios from "axios";
 import { CardWindow, Card, Form, FormHeading, Input, Button, Error } from "./AuthForm";
@@ -9,68 +9,72 @@ const Login = props => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [auth, setAuth] = useContext(AuthContext);
+  const [token, setToken] = useContext(AuthContext);
   
-
   const postLogin = () => {
+    
+
     axios
       .post("http://localhost:4000/api/login", {
         email,
         password
       })
       .then(res => {
-        if (res.status === 200) {
+        
           setAuth({
            ...auth,
            email: res.data.email,
            role: res.data.role,
            isLoggedIn: res.data.token ? true: false,
+           token: res.data.token,
            isAdmin: res.data.role === 'admin' ? true: false,
-         })
-         alert("Successfully Logged In!")
-         
-        }
+         });
          
       })
       .catch(error => {
         console.log({ error });
         setIsError(true);
       });
+     
   };
-  
-          
-
- 
-  
-
+  console.log(auth.token)
+  const logout = () => {
+    auth.token = '';
+    console.log(auth.token)
+  }
   return (
-      <CardWindow>
-        <Card>
-          <Form>
-            <FormHeading>LOG-IN FORM</FormHeading>
-            <Input
-              type="email"
-              value={email}
-              onChange={(e) => {
-                setEmail(e.target.value);
-              }}
-              placeholder="email address"
-            />
-            <Input
-              type="password"
-              value={password}
-              onChange={(e) => {
-                setPassword(e.target.value);
-              }}
-              placeholder="password"
-            />
+    <CardWindow>
+      <Card>
+        <Form>
+          <FormHeading>LOG-IN FORM</FormHeading>
+          <Input
+            type="email"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            placeholder="email address"
+          />
+          <Input
+            type="password"
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            placeholder="password"
+          />
+          {!auth.token ? (
             <Button onClick={postLogin}>Log In</Button>
-          </Form>
-          <NavLink to="/register">Don't have an account?</NavLink>
-          {isError && (
-            <Error>The email or password provided was incorrect!</Error>
+          ) : (
+            <Button onClick={logout}>Log Out</Button>
           )}
-        </Card>
-      </CardWindow>
+        </Form>
+        <NavLink to="/register">Don't have an account?</NavLink>
+        {isError && (
+          <Error>The email or password provided was incorrect!</Error>
+        )}
+      </Card>
+    </CardWindow>
   );
 };
 
