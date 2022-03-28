819a1024434dec164381d62c6d60bfdeef48ec63
Greyflanel <47545416+Greyflanel@users.noreply.github.com>
2022-03-28 12:29:30 -0500

    bugfix didn't work in production

--- src/components/Products.js
+++ src/components/Products.js
@@ -13,7 +13,7 @@ const Products = (props) => {
   
   useEffect(() => {
     axios
-      .get('https://api.computerspartselectronics.com/products' || 'http://localhost:3000')
+      .get('https://api.computerspartselectronics.com/products')
       .then(response => {
         
         setProd(response.data)
