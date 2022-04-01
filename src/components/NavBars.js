18db4a3bc61bd9e7505d266b6af799885dbea392
Greyflanel <47545416+Greyflanel@users.noreply.github.com>
2022-04-01 02:13:41 -0500

    new logo png

--- src/components/NavBars.js
+++ src/components/NavBars.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "../styles/index.css";
 import { useAuth0 } from "@auth0/auth0-react";
-import logo from "../assets/logo3-removebg.png";
+import logo from "../assets/Untitled-1.png";
 import {
   NavItem,
   UncontrolledDropdown,
