7e85cafaa04891687563f82aea9dcd382d911522
Greyflanel <47545416+Greyflanel@users.noreply.github.com>
2022-05-03 12:14:10 -0500

    new changes

--- src/components/NavBars.js
+++ src/components/NavBars.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "../styles/index.css";
 import { useAuth0 } from "@auth0/auth0-react";
-import logo from "../assets/Sharif-Logo7.png";
+import logo from "../assets/Sharif-Logo100.png";
 import User from "./User.js";
 import {
   NavItem,
