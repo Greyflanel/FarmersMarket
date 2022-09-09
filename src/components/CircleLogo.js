0453d86928e4f46c3cc68c86719dc9538d44a7ae
Greyflanel <47545416+Greyflanel@users.noreply.github.com>
2022-09-09 16:43:53 -0500

    j

---
+++ src/components/CircleLogo.js
@@ -0,0 +1,65 @@
+// import React, {useRef, useEffect} from "react";
+// import lionText from "../assets/Lion-Logo-Text.png";
+// import lion from "../assets/new-hi-res.png";
+// import styled from "styled-components";
+// import { devices } from "./devices.js";
+// import { gsap } from "gsap";
+// import "../styles/index.css";
+
+// export default function CircleLogo() {
+// const circleRef = useRef(null);
+//   useEffect(() => {
+    
+//       let tl = gsap.timeline({ repeat: 0 });
+//       if (circleRef) {
+//       tl.set(circleRef.current, {
+//         autoAlpha: 0,
+//         clipPath: "circle(0% at 50% 50%)"
+//       }).to(
+//         circleRef.current,
+//         {
+//           delay: 0.5,
+        
+//           clipPath: "circle(50% at 50% 50%)",
+//           duration: 2,
+//           ease: "sine.out",
+//         }
+//       );
+//       } else {
+//         return;
+//     }
+//   }, []);
+
+//   return ( 
+//     <LogoContainer className="logo-container" ref={circleRef}>
+//         <LogoText src={lionText} alt="sharif logo text" className="logo-text" />
+//         <Lion src={lion} alt="lion logo" className="lion" />
+//     </LogoContainer>
+//   );
+// }
+
+// const LogoContainer = styled.div`
+//   position: relative;
+//   display: flex;
+//   width: 350%;
+//   justify-content: center;
+//   align-items: center;
+//   /* border: 1px solid red; */
+//   background-color: transparent !important;
+  
+// `
+
+// const Lion = styled.img`
+//   position: relative;
+//   width: 62%;
+//   padding-top: 0.8vh;
+//   /* border: 1px solid red; */
+  
+// `
+
+// const LogoText = styled.img`
+//   position: absolute;
+//   width: 100%;
+//   filter: drop-shadow(1px 0.8px 1px var(--primary));
+// `
+
--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,18 +1,209 @@
 import React from "react";
 import "../styles/index.css";
-import video from "../assets/CPU.mp4";
-
+import { productList } from "./server.js";
+import circuit from "../assets/circuit-image.jpg";
+// import CircleLogo from "../components/CircleLogo.js";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { TextPlugin } from "gsap/TextPlugin";
+gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(TextPlugin);
 const LandingPage = () => {
+  useEffect(() => {
+    let tl2 = gsap.timeline({
+      // yes, we can add it to an entire timeline!
+      // scrollTrigger: {
+      //   trigger: ".first-section",
+      // pin: true, // pin the trigger element while active
+      // anticipatePin: 1,
+      // start: "top top", // when the top of the trigger hits the top of the viewport
+      // end: "-=100", // end after scrolling 500px beyond the start
+      // scrub: 10, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
+      // snap: {
+      //   snapTo: "labels", // snap to the closest label in the timeline
+      //   duration: { min: 0.2, max: 3 }, // the snap animation should be at least 0.2 seconds, but no more than 3 seconds (determined by velocity)
+      //   delay: 0.2, // wait 0.2 seconds from the last scroll event before doing the snapping
+      //   ease: "power1.inOut", // the ease of the snap animation ("power3" by default)
+      // },
+      // },
+  //   });
+    tl2.play();
+    let tl3 = gsap.timeline({ repeat: 0 });
+    let tl4 = gsap.timeline({
+      repeat: "-1",
+    });
+    let tl5 = gsap.timeline({
+      repeat: 0,
+      zIndex: 9999,
+      
+    });
+
+    tl3.to(".overlay2", {
+      autoAlpha: 1,
+    });
+
+    tl4.to(".logo-text", {
+      duration: 5000,
+      rotation: "7200deg",
+    });
+
+    tl5
+      .set(".title", {
+        letterSpacing: "-0.75em",
+        ease: "power4.inOut",
+      })
+      .set(".logo-container", {
+        autoAlpha: 1,
+        duration: 1.5,
+        
+      })
+      .to(".title", {
+        autoAlpha: 1,
+        duration: 1.5,
+        letterSpacing: "-0.0",
+      })
+      .to(
+        ".title",
+        {
+          opacity: 1,
+        },
+        "+=10"
+      )
+  }, []);
+
   return (
-    <>
-    <section className="showcase">
-      <video muted loop autoPlay>
-        <source src={video} type="video/mp4"/>
-      </video>
-      <div className="overlay"></div>
-    </section>
-   
-    </>
+    <div className="landing-page-container">
+      <section className="first-section">
+        <div className="title-container">
+          <h1 className="title">
+          <span className="row1">SHARIF'S</span>
+          <br />
+          Computer <br />
+          Parts and <br />
+          Electronics
+        </h1>
+        </div>
+        
+        
+        <div className="custom-shape-divider-bottom-1645215428">
+          <svg
+            data-name="Layer 1"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 1200 120"
+            preserveAspectRatio="none"
+          >
+            <path
+              d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z"
+              opacity="0"
+              className="shape-fill"
+            ></path>
+            <path
+              d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z"
+              opacity="1"
+              className="shape-fill"
+            ></path>
+          </svg>
+        </div>
+        
+      </section>
+
+      <section className="second-section">
+        <div className="custom-shape-divider-top-1645218211">
+          <svg
+            data-name="Layer 1"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 1200 120"
+            preserveAspectRatio="none"
+          >
+            <path
+              d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z"
+              opacity=".5"
+              className="shape-fill"
+            ></path>
+            <path
+              d="M0,0V15.81C13,36.92,27.64,56.86,47.69,72.05,99.41,111.27,165,111,224.58,91.58c31.15-10.15,60.09-26.07,89.67-39.8,40.92-19,84.73-46,130.83-49.67,36.26-2.85,70.9,9.42,98.6,31.56,31.77,25.39,62.32,62,103.63,73,40.44,10.79,81.35-6.69,119.13-24.28s75.16-39,116.92-43.05c59.73-5.85,113.28,22.88,168.9,38.84,30.2,8.66,59,6.17,87.09-7.5,22.43-10.89,48-26.93,60.65-49.24V0Z"
+              opacity=".5"
+              className="shape-fill"
+            ></path>
+            <path
+              d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z"
+              className="shape-fill"
+            ></path>
+          </svg>
+        </div>
+      </section>
+
+      <section className="third-section">
+        <div className="custom-shape-divider-bottom-1645215428">
+          <svg
+            data-name="Layer 1"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 1100 120"
+            preserveAspectRatio="none"
+          >
+            <path
+              d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z"
+              opacity=".5"
+              className="shape-fill"
+            ></path>
+            <path
+              d="M0,0V15.81C13,36.92,27.64,56.86,47.69,72.05,99.41,111.27,165,111,224.58,91.58c31.15-10.15,60.09-26.07,89.67-39.8,40.92-19,84.73-46,130.83-49.67,36.26-2.85,70.9,9.42,98.6,31.56,31.77,25.39,62.32,62,103.63,73,40.44,10.79,81.35-6.69,119.13-24.28s75.16-39,116.92-43.05c59.73-5.85,113.28,22.88,168.9,38.84,30.2,8.66,59,6.17,87.09-7.5,22.43-10.89,48-26.93,60.65-49.24V0Z"
+              opacity=".6"
+              className="shape-fill"
+            ></path>
+            <path
+              d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z"
+              className="shape-fill"
+            ></path>
+          </svg>
+        </div>
+        {/* <Svg /> */}
+        {/* <h2>Third-Section</h2> */}
+      </section>
+      <section className="fourth-section">
+        <div className="custom-shape-divider-top-1645218211">
+          <svg
+            data-name="Layer 1"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 1200 120"
+            preserveAspectRatio="none"
+          >
+            <path
+              d="M0,0V46.29c47.79,22.2,103.59,32.17,158,28,70.36-5.37,136.33-33.31,206.8-37.5C438.64,32.43,512.34,53.67,583,72.05c69.27,18,138.3,24.88,209.4,13.08,36.15-6,69.85-17.84,104.45-29.34C989.49,25,1113-14.29,1200,52.47V0Z"
+              opacity=".5"
+              className="shape-fill"
+            ></path>
+
+            <path
+              d="M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z"
+              className="shape-fill"
+            ></path>
+          </svg>
+        </div>
+        {/* <div className="cardinal">
+          <div className="face face1">
+            <div className="content">
+              <i className="fab fa-windows"></i>
+              <h3>Windows</h3>
+            </div>
+          </div>
+          <div className="face face2">
+            <div className="content">
+              <p>
+                {" "}
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde ab
+                repudiandae, explicabo voluptate et hic cum ratione a. Officia
+                delectus illum perferendis maiores quia molestias vitae fugiat
+                aspernatur alias corporis?
+              </p>
+              <a href="#" type="button">
+                Read More
+              </a>
+            </div>
+          </div>
+        </div> */}
+      </section>
+    </div>
   );
 };
 
