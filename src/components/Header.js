4e3ec83764980ab48b1fafe9c24a466f2fc5f1f4
Greyflanel <47545416+Greyflanel@users.noreply.github.com>
2022-09-14 00:45:38 -0500

    removed background from header logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "../styles/index.css";
 import { useAuth0 } from "@auth0/auth0-react";
-import logo from "../assets/good-cropped.jpg";
+import logo from "../assets/image.png";
 import User from "./User.js";
 import {
   NavItem,
