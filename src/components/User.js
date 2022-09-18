1472c77ff1ed447cfde947858f723cd7cc827dd0
Greyflanel <47545416+Greyflanel@users.noreply.github.com>
2022-09-17 20:17:59 -0500

    adjusted username display

--- src/components/User.js
+++ src/components/User.js
@@ -4,16 +4,17 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 const User = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
-
+  // 
   if (isLoading) {
     return <div></div>;
   }
-
+const userName = user.name.replace(/@.*/, "");
+  
   return (
     isAuthenticated && (
       <div className="user-profile">
         <h2>Welcome</h2>
-        <h3>{user.name}</h3>
+        <h3>{userName}</h3>
       </div>
     )
   );
