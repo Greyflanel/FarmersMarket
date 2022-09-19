9872cb97a28006bee3514b71174caacb245e3467
Greyflanel <47545416+Greyflanel@users.noreply.github.com>
2022-09-19 14:55:14 -0500

    Revert "replaced useHistory with useNavigate"
    This reverts commit 177a75bc109eeda94f0c139f8b81a34c7b219931.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom"; 
+import { BrowserRouter as Router } from "react-router-dom";
 import "./styles/index.css";
 import App from "./App";
 import { Auth0Provider } from "@auth0/auth0-react";
-import { useNavigate } from "react-router-dom";
+
 import { getConfig } from "./config";
 
-const onRedirectCallback = (appState) => {
-  useNavigate.push(
-    appState && appState.returnTo ? appState.returnTo : window.location.pathname
-  );
-};
+// const onRedirectCallback = (appState) => {
+//   history.push(
+//     appState && appState.returnTo ? appState.returnTo : window.location.pathname
+//   );
+// };
 
 // Please see https://auth0.github.io/auth0-react/interfaces/auth0_provider.auth0provideroptions.html
 // for a full list of the available properties on the provider
@@ -21,8 +21,8 @@ const providerConfig = {
   domain: config.domain,
   clientId: config.clientId,
   ...(config.audience ? { audience: config.audience } : null),
-  redirectUri: window.location.origin,
-  onRedirectCallback,
+  // redirectUri: window.location.origin,
+  // onRedirectCallback,
 };
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
