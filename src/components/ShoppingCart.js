4bbb54ff976540eacedf1832e2b6891687a37dd8
Greyflanel <richardlovelace811@gmail.com>
2022-09-20 23:07:48 -0500

    hoping

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -12,7 +12,7 @@ const ShoppingCart = () => {
 
   useEffect(() => {
     // Create PaymentIntent as soon as the page loads
-    fetch("http://localhost:4000/create-payment-intent", {
+    fetch("https://api.computerspartselectronics.com/create-payment-intent", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(cart),
